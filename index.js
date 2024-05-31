d3d6846ae8ab85cbc3476eb02b0e76ac09d9a5ea
AshleyEvonne <ashleyevonne345@gmail.com>
2024-05-31 15:40:51 -0400

    Added form with validation, BOM, and Modified html

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const main = document.querySelector("main");
 const body = document.querySelector("body");
 const h1 = document.querySelector("h1")
 // const nav = document.querySelector("nav");
-body.style.backgroundColor = ('black');
+body.style.backgroundColor = ('beige');
 // console.log(main)
+// document.getElementsByTagName("p").innertext = "RECORD & PLAY"
 //  h1.innerText = "RECORD"
 //  main.appendChild(h1)
 let vid = document.getElementById("myVideo")
@@ -12,14 +13,13 @@ vid.style.marginBottom = "40px"
 vid.style.paddingBottom= "50px"
 
 
-
 const navBar = document.querySelector("#top-bar");
 // console.log(navBar)
 
-
 navBar.style.height = "50px"
 navBar.style.width ="100%"
-navBar.style.background = "black"
+navBar.style.background = "beige"
+navBar.style.border = "outset, 10px, darkgoldenrod"
 navBar.classList.add ("flex-around")
 // navBar.style.position ="absolute"
 console.log(navBar)
@@ -39,7 +39,7 @@ navLinks.forEach((link) => {
     aElement.textContent = link.text;
     //append new element to the navbar element
     navBar.appendChild(aElement);
-    aElement.style.color = "white"
+    aElement.style.color = "darkgoldenrod"
 });
 navBar.style.margin = "3px";
 
@@ -61,11 +61,50 @@ let imgArr = [
 ]
 imgArr.forEach(element => {
    const pics = document.createElement("img"); //creating an img element
-   const btn = document.createElement("button");
+//    const btn = document.createElement("button");
    pics.src = element.URL // selecting photo, allowing the photo to show
    main.appendChild(pics) // attaching "pics" to the "main" element
-   pics.appendChild(btn)
+//    pics.appendChild(btn)
     pics.style.height = "20vh"
     pics.style.border = "outset, 10px, darkgoldenrod "
     pics.style.margin = "40px"
+    // pics.addEventListener("mouseenter",(event) => {
+    //     let timeFade = pics.style.opacity = "0.8" 
+    //     timeFade.setTimeout((1000) => {
+            
+    //     }, timeout);
+
+    // })
 });
+
+const firstName = document.getElementById("fname")
+const lastName = document.getElementById("lname")
+const email = document.getElementById("email")
+
+
+
+function validateForm() {
+    let f = document.forms["lname"]["fname"]["email"].value;
+    console.log(f)
+    if (f == "") {
+    alert("Name must be filled out");
+    return false;
+    }
+    }
+const form = document.getElementById("form")
+form.addEventListener("submit",( e) =>{
+    e.preventDefault()
+    console.log(firstName.value);
+    console.log(lastName.value);
+    console.log(email.value);
+    if (firstName.value == "" || lastName.value == "" || email.value == ""){
+    alert("Please fill out field")
+    } else {
+        confirm(`Hello ${firstName.value}, Are you sure you want to subscribe?`)
+    }
+
+});
+document.querySelector("#title").innerHTML = "<h1>Subcribe To News Letter</h1>"
+// let formTitle = document.createElement("h1");
+// formTitle.innerText="Subscribe To News Letter"
+// formTitle.appendChild(form);
